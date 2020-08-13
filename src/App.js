3390a4b485022500610c0c0ced31aec8c2190500
Nick D <nddudash@fouytan.local>
2020-08-13 13:18:28 -0600

    before project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./components/welcome/Welcome.js";
+import Clock from "./components/clock/Clock.js";
+import Contact from "./components/contact/Contact.js"
+import {Route} from "react-router-dom"
+import Navigation from "./components/navigation/Navigation.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       A E S T H E T I C
+//       <Welcome name="Baby" />
+//       <Clock />
+//       <Contact />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* define our routes */}
+        <Navigation />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="eric" />}
+        />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
